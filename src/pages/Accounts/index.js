6485246d4365b0f441f1d9e832ec0e4c2aa0cd52
Phuoc Dinh <phuocdinh462@gmail.com>
2023-07-09 10:00:41 +0700

    refactor: add filter

--- src/pages/Accounts/index.js
+++ src/pages/Accounts/index.js
@@ -1,7 +1,36 @@
+import classes from "./accounts.module.scss";
+import Filter from "../../components/Layout/DefaultLayout/Filter/index";
+
+const filters = [
+  {
+    title: "Khách hàng",
+    color: "#0094e8",
+    icon: <i className="fa-regular fa-user"></i>,
+    quantity: 100,
+  },
+  {
+    title: "Tài xế",
+    color: "#00E878",
+    icon: <i className="fa-regular fa-user"></i>,
+    quantity: 100,
+  },
+  {
+    title: "Tổng đài",
+    color: "#E8A700",
+    icon: <i className="fa-regular fa-user"></i>,
+    quantity: 100,
+  },
+];
+
 function Accounts() {
   return (
     <>
       <h1>Tài khoản</h1>
+      <div className={classes["filter-container"]}>
+        {filters.map((item, index) => (
+          <Filter key={index} title={item.title} icon={item.icon} color={item.color} quantity={item.quantity}></Filter>
+        ))}
+      </div>
     </>
   );
 }
