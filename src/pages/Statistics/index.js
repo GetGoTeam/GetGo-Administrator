b75efea325acb9a17806a5fbe5f994d870dab254
Phuoc Dinh <phuocdinh462@gmail.com>
2023-07-10 23:37:34 +0700

    refactor: add StatisticsTable

--- src/pages/Statistics/index.js
+++ src/pages/Statistics/index.js
@@ -1,7 +1,10 @@
+import StatisticsTable from "../../components/Layout/DefaultLayout/StatisticTable";
+
 function Statistics() {
   return (
     <>
       <h1>Thống kê</h1>
+      <StatisticsTable yesterdayRevenue={2000000} todayRevenue={2100000} canceledTrips={75} completedTrips={1248} />
     </>
   );
 }
